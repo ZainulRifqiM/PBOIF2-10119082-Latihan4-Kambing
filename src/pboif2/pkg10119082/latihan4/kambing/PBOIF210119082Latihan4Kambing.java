9fac8088e6f5f0a22d86a43f2d195abeda2b60da
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 16:47:10 +0700

    PBOIF2-10119082-Latihan4-Kambing

---
+++ src/pboif2/pkg10119082/latihan4/kambing/PBOIF210119082Latihan4Kambing.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan4.kambing;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Kambing
+ */
+public class PBOIF210119082Latihan4Kambing {
+    
+    public void tambahKambing() {
+        // Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOIF210119082Latihan4Kambing kambingJantan = new PBOIF210119082Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
